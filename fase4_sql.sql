f7c77a928e59007d715ab4e2e4516b7431879f4f
ailaendo <ailaendo@gmail.com>
2024-11-08 15:45:35 -0300

    first commit

---
+++ fase4_sql.sql
@@ -0,0 +1,24 @@
+CREATE TABLE anp.preco_combustivel(
+		regiao 				varchar(255)
+		,estado				varchar(255)
+		,municipio			varchar(255)
+		,revenda			varchar(255)
+		,cnpj				varchar(255)
+		,nome_rua			varchar(255)	
+		,numero_rua			varchar(255)
+		,complemento		varchar(255)
+		,bairro				varchar(255)
+		,cep				varchar(255)
+		,produto			varchar(255)
+		,data_coleta		date
+		,valor_venda		float
+		,unidade_medida		varchar(255)
+		,bandeira			varchar(255)
+
+)
+
+SELECT * FROM anp.preco_combustivel;
+
+SELECT count(*) FROM anp.preco_combustivel;
+
+SELECT pg_encoding_to_char(encoding) FROM pg_database WHERE datname = 'ANP';
