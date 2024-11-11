6d82c04cc12198eefaef84146d73a65239598fe1
ailaendo <ailaendo@gmail.com>
2024-11-11 17:27:39 -0300

    atuliazando pastas

---
+++ Tech/tech4_preco_petroleo.sql
@@ -0,0 +1,11 @@
+CREATE TABLE fob.preco_petroleo (
+				data_coletado varchar(255),
+				preco		  varchar(255),
+				abertura	  varchar(255),
+				maxima 		varchar(255),
+				minima varchar(255),
+				volume varchar(255),
+				variacao varchar(255)
+) 
+
+DROP TABLE fob.preco_petroleo
